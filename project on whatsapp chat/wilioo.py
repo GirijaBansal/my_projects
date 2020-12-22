c27059a9891fb656ae600b8ebfc47b859aebec44
GirijaBansal <67459702+GirijaBansal@users.noreply.github.com>
2020-12-22 11:42:27 +0530

    Update wilioo.py

--- project on whatsapp chat/wilioo.py
+++ project on whatsapp chat/wilioo.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client 
  
 account_sid = 'ACec33ea0a8ba567843473a3ea67e9fecf' 
-auth_token = '5074b6c51131a1f9b89b9750942a19a5' 
+auth_token = '5074b6c51131a1f9b89b9750942a19a000000000' 
 client = Client(account_sid, auth_token) 
 def job_function(): 
         message = client.messages.create( 
                               from_='whatsapp:+14155238886',  
                               body='Gudmorning ',      
-                              to='whatsapp:+918923397522' 
+                              to='whatsapp:+91000000000000' 
                           ) 
  
         print(message.sid)
